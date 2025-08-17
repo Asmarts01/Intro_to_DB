601ccbad6628cd0ead5d11f2539a20060691cb32
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-08-17 20:08:13 +0100

    fix: correct exception handling for MySQL error and ensure proper script execution

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,10 +13,13 @@ def create_database():
         # create database
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully. ")
-    except mysql.connector.error as err:
+    except mysql.connector.Error as err:
         print(f"Error: {err}")
     finally:
         if cursor:
             cursor.close()
             if connection:
                 connection.close()
+                
+if __name__ == "__main__":
+    create_database()
