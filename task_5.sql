9fb9c7c0140f7ed51488ce5e480d4859e6a37df8
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-08-17 23:19:01 +0100

    chore: add initial SQL script for inserting a customer record in the database

---
+++ task_5.sql
@@ -0,0 +1,12 @@
+-- Write a script that inserts a single row in the table customer (database alx_book_store) in your MySQL server.
+
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = 'Cole Baidoo',
+email = 'cbaidoo@sandtech.com',
+address = '123 Happiness Ave.',
+
