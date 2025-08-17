5c581f8ce91c471c8b6d782732cd3658d7cb29ac
Aigbogun Anointed <aigbogunanointed71@gmail.com>
2025-08-17 23:20:27 +0100

    fix: correct SQL syntax by removing trailing comma in address update

--- task_5.sql
+++ task_5.sql
@@ -8,5 +8,5 @@ VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
 ON DUPLICATE KEY UPDATE
 customer_name = 'Cole Baidoo',
 email = 'cbaidoo@sandtech.com',
-address = '123 Happiness Ave.',
+address = '123 Happiness Ave.';
 
